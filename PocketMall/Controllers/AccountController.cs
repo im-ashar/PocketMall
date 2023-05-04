72e2c871031fdf86f232e9494c9d9543f37bb541
im-ashar <muhammadashar.401@gmail.com>
2023-05-05 02:22:52 +0500

    Add project files.

---
+++ PocketMall/Controllers/AccountController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PocketMall.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Signup()
+        {
+            return View();
+        }
+        public IActionResult Signin()
+        {
+            return View();
+        }
+    }
+}
